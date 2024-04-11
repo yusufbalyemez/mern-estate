d85ee961c5df65751b4cbb8f9f6686576796d6fa
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-04-11 18:11:33 +0300

    create middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+//next'i yazdığımız hata middleware'ini kullanmak için oluşturdum.
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -10,6 +13,8 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("User created successfully");
   } catch (err) {
-    res.status(500).json(err.message);
+    /* res.status(500).json(err.message); */
+    //oluşturulan hata middleware'i kullanıldı.
+    next(err); 
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,27 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("Connected to MongoDB!")
-}).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!');
-}
-);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter)
+//Hata Middleware'i. Bunu her projede kullabilirsin.
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    succes: false,
+    statusCode,
+    message,
+  });
+});
