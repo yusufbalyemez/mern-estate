b076688039b2205028906be19d1beab62ec543f2
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-04-29 12:51:09 +0300

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -62,17 +62,26 @@ export const google = async (req, res, next) => {
           req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-8),
         email: req.body.email,
-        password: hashedPassword, 
+        password: hashedPassword,
         avatar: req.body.photo,
       });
       await newUser.save();
-      const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET)
-      const {password: pass, ...rest} = newUser._doc;
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
       res
         .cookie("access_token", token, { httpOnly: true, secure: true }) // HTTPS için secure eklenmeli
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res) => {
+  try {
+    res.clearCookie('acces_token');
+    res.status(200).json('User has been logged out!');
   } catch (error) {
     next(error);
   }
